1a3906f2f8e2b214cf05c30b5611d7ffb66c548e
LasseAafeldt <superlla@hotmail.com>
2017-10-03 14:20:37 +0200

    camera rotation script

---
+++ Køkkenmødding/Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityX = 15F;
+    public float sensitivityY = 15F;
+
+    float rotationY = 0F;
+
+    void LateUpdate()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            if (axes == RotationAxes.MouseXAndY)
+            {
+                float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+                rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+                transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+            }
+            else
+            {
+                rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+                transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+            }
+        }
+    }
+}
