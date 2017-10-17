2816c9282733dda81f3134b7234be48e5b94a980
Johan <jwkr14@student.aau.dk>
2017-10-17 10:32:15 +0200

    Added scripts for gyroscope (the working version is number 3) and raycast-script that uses touch.

---
+++ Køkkenmødding/Assets/Scripts/UI/Gyroscope/CameraTouch.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTouch : MonoBehaviour {
+
+	private Camera cam;
+	private float xAngle = 0.0f;
+	private float yAngle = 0.0f;
+	private float xAngleTemp = 0.0f;
+	private float yAngleTemp = 0.0f;
+	private Vector3 secondPoint;
+	private Vector3 firstPoint;
+
+	// Use this for initialization
+	void Start () {
+		cam = GetComponent<Camera>();
+		xAngle = 0.0f;
+		yAngle = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0) {
+		if (Input.GetTouch(0).phase == TouchPhase.Began)
+		{
+			firstPoint = Input.GetTouch (0).position;
+			xAngleTemp = xAngle;
+			yAngleTemp = yAngle;
+		}
+		if (Input.GetTouch (0).phase == TouchPhase.Moved) {
+				secondPoint = Input.GetTouch (0).position;
+				xAngle = xAngleTemp + (secondPoint.x - firstPoint.x) * 180.0f / Screen.width;
+				yAngle = yAngleTemp + (secondPoint.y - firstPoint.y) * 180.0f / Screen.height;
+				Vector3 vec = new Vector3 (yAngle, xAngle, 0.0f);
+				cam.transform.Rotate(vec);
+			}
+		}
+	}
+}
