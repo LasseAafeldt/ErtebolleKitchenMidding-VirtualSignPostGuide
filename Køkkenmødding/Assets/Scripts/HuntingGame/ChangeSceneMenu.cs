476fb939b00c2084235a25345c39bbe071ac3984
Johan <jwkr14@student.aau.dk>
2017-11-22 10:24:15 +0100

    + loging script

--- Køkkenmødding/Assets/Scripts/HuntingGame/ChangeSceneMenu.cs
+++ Køkkenmødding/Assets/Scripts/HuntingGame/ChangeSceneMenu.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class ChangeSceneMenu : MonoBehaviour {
     public Button button;
+	public static float timePlay = 0.0f;
 	// Use this for initialization
 	void Start () {
         button.onClick.AddListener(delegate { click(); });
@@ -14,6 +15,8 @@ public class ChangeSceneMenu : MonoBehaviour {
     void click()
     {
 		Debug.Log (Time.timeSinceLevelLoad);
+		timePlay = Time.timeSinceLevelLoad;
+		PlayerPrefs.SetFloat ("game Time", timePlay);
         SceneManager.LoadScene("Midding"); //if main scene name change change here aswell.
     }
   
--- Køkkenmødding/Assets/Scripts/UI/Options/AudioHandler.cs
+++ Køkkenmødding/Assets/Scripts/UI/Options/AudioHandler.cs
@@ -39,4 +39,5 @@ public class AudioHandler : MonoBehaviour {
             }
         }
     }
+
 }
