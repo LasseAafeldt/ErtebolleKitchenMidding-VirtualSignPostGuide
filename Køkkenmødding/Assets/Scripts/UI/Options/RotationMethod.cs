6081e71374a87c82f93eb171eb71a47280de8bf3
Lasse Aafeldt <superlla@hotmail.com>
2018-03-19 08:07:57 +0100

    attempt at fixing touch control (now disable/enable gyroscope when switching rotation method)

--- Køkkenmødding/Assets/Scripts/UI/Options/RotationMethod.cs
+++ Køkkenmødding/Assets/Scripts/UI/Options/RotationMethod.cs
@@ -7,7 +7,7 @@ public class RotationMethod : MonoBehaviour
 {
 
     public Button button;
-    public bool gyro;
+    public static bool gyro;
     public Color selected;
     public Color deSelected;
     // Use this for initialization
@@ -16,6 +16,7 @@ public class RotationMethod : MonoBehaviour
         selected = new Color(0.58F, 0.58F, 0.58F, 0.58F);
         deSelected = new Color(0, 0, 0, 0);
         gyro = true;
+        CheckGyro();
         GameObject.Find("Gyro").GetComponent<RawImage>().color = selected;
         button.onClick.AddListener(delegate { click(); });
     }
@@ -23,17 +24,36 @@ public class RotationMethod : MonoBehaviour
     public void setSwipeControl()
     {
         gyro = false;
+        CheckGyro();
         Debug.Log("finger");
 
         GameObject.Find("Finger").gameObject.GetComponent<RawImage>().color = selected;
         GameObject.Find("Gyro").gameObject.GetComponent<RawImage>().color = deSelected;
     }
 
+    public bool CheckGyro()
+    {
+        if(gyro == false)
+        {
+            Input.gyro.enabled = false;
+            return false;
+        }
+        if (SystemInfo.supportsGyroscope)
+        {
+            Input.gyro.enabled = true;
+            return true;
+        }
+        //should probably pull up the gyro UI again here
+        return false;
+    }
+
+
     void click()
     {
         if (button.CompareTag("Gyro"))
         {
             gyro = true;
+            CheckGyro();
             Debug.Log("gyro");
 
             gameObject.transform.parent.gameObject.GetComponent<RawImage>().color = selected;
@@ -44,6 +64,7 @@ public class RotationMethod : MonoBehaviour
         if (button.CompareTag("Finger"))
         {
             gyro = false;
+            CheckGyro();
             Debug.Log("finger");
 
             gameObject.transform.parent.gameObject.GetComponent<RawImage>().color = selected;
