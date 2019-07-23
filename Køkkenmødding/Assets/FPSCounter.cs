aa7e6fb1cdc8b58e42397db638bc2c6e0f77a660
Lasse Aafeldt <superlla@hotmail.com>
2019-07-23 23:56:40 +0200

    fixed camera controls (including drift, it's good enough but not perfect)

---
+++ Køkkenmødding/Assets/FPSCounter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCounter : MonoBehaviour {
+
+    Text tex;
+
+	void Start () {
+        tex = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float fps = 1 / Time.deltaTime;
+        tex.text = "FPS = " + fps.ToString();
+	}
+}
--- Køkkenmødding/Assets/Scripts/UI/Gyroscope/CameraControl3.cs
+++ Køkkenmødding/Assets/Scripts/UI/Gyroscope/CameraControl3.cs
@@ -2,52 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraControl3 : MonoBehaviour {
+public class CameraControl3 : MonoBehaviour
+{
+    float driftCorrection = 5f;
 
-	private bool gyroEnabled;
-	private Gyroscope gyro;
+    private bool gyroEnabled;
+    private Gyroscope gyro;
 
-	private GameObject cameraContainer;
-	private Quaternion rot;
-    private Quaternion gyroTranslation;
-    private Quaternion previousGyroAttitude;
+    private GameObject cameraContainer;
+    private Quaternion rot;
+    private Quaternion previousAtt;
 
-	// Use this for initialization
-	void Start () {
-		cameraContainer = new GameObject ("Camera Containter");
-		cameraContainer.transform.position = transform.position;
-		transform.SetParent (cameraContainer.transform);
-		gyroEnabled = EnableGyro ();
-		Screen.orientation = ScreenOrientation.LandscapeLeft;
-        previousGyroAttitude = gyro.attitude;
-	}
+    // Use this for initialization
+    void Start()
+    {
+        cameraContainer = new GameObject("Camera Containter");
+        cameraContainer.transform.position = transform.position;
+        transform.SetParent(cameraContainer.transform);
+        gyroEnabled = EnableGyro();
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+        try { previousAtt = gyro.attitude; }
+        catch { }
+        Vector3 test = new Vector3(0.1f, 0.2f, 0.3f);
+        Debug.Log("length = " + test.magnitude);
+    }
 
-	// Update is called once per frame
-	void Update () {
-		if (gyroEnabled) {
-            //transform.localRotation = gyro.attitude * rot;
-            Quaternion currentGyroAttitude = gyro.attitude;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (gyroEnabled)
+        {
+            Quaternion currentAtt = gyro.attitude;
+            //Debug.Log("Rot rate = "+ gyro.rotationRateUnbiased);
+            if (Quaternion.Angle(currentAtt, previousAtt) > driftCorrection * Time.deltaTime && gyro.rotationRateUnbiased.magnitude > 0.3f)
+            {
+                Quaternion tempNew = currentAtt * rot;
+                Quaternion tempOld = previousAtt * rot;
+                Quaternion targetTranslation = Quaternion.Inverse(tempOld) * tempNew;
+                Quaternion targetRot = transform.localRotation * targetTranslation;
+                transform.localRotation = targetRot;
+            }
 
-            gyroTranslation = currentGyroAttitude * Quaternion.Inverse(previousGyroAttitude);
 
 
-            transform.localRotation = transform.localRotation * gyroTranslation;
-            previousGyroAttitude = gyro.attitude;
-		}
-	}
-	private bool EnableGyro(){
-		if (SystemInfo.supportsGyroscope) {
-			gyro = Input.gyro;
-			gyro.enabled = true;
+            previousAtt = gyro.attitude;
+        }
+    }
+    private bool EnableGyro()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
 
-            //cameraContainer.transform.rotation = Quaternion.Euler (90f, 90f, 0f);
+            //cameraContainer.transform.rotation = Quaternion.Euler(90f, 90f, 0f);
             cameraContainer.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
-            rot = new Quaternion (0, 0, 1, 0);
+            rot = new Quaternion(0, 0, 1, 0);
 
-            
-			return true;
-		}
-		return false;
-	}
+            return true;
+        }
+        return false;
+    }
 
 }
